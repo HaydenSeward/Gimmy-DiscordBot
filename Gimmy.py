b2b0a297532eecd63189c82dd0247d1443360ee6
HaydenSeward <haydenseward78@gmail.com>
2018-08-18 17:36:51 -0700

    First

---
+++ Gimmy.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+from os import environ
+from boto.s3.connection import S3Connection
+
+TOKEN = S3Connection(environ["TOKEN"])
+bot = commands.Bot(command_prefix = "!")
+
+@bot.command()
+async def test(ctx):
+    f = open("UserData", "w")
+    f.write("Test")
+    f.close()
+    await ctx.send('Maybe {0}'.format(ctx.author))
+
+bot.run(TOKEN)
