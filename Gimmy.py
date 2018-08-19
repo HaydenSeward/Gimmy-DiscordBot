44fa6317eb5a03c70ec7431aac0327847bb7f55e
HaydenSeward <haydenseward78@gmail.com>
2018-08-18 18:05:30 -0700

    Update Gimmy.py

--- Gimmy.py
+++ Gimmy.py
@@ -2,7 +2,7 @@ from discord.ext import commands
 from os import environ
 from boto.s3.connection import S3Connection
 
-TOKEN = S3Connection(environ["TOKEN"])
+TOKEN = S3Connection(os.environ["TOKEN"])
 bot = commands.Bot(command_prefix = "!")
 
 
